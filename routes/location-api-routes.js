9ef24203d99d1c365d562f8f5396876165a793c3
Lavina91 <65462918+Lavina91@users.noreply.github.com>
2021-03-03 13:07:33 -0700

    added code for create and delete routes in all routes files

--- routes/location-api-routes.js
+++ routes/location-api-routes.js
@@ -11,12 +11,20 @@ module.exports = (app) => {
     app.get('/api/locations/:id', (req, res) => { });
 
     // CREATE NEW LOCATION
-    app.post('/api/locations', (req, res) => { });
+    app.post('/api/locations', (req, res) => { 
+        db.Location.create(req.body).then((dbLocation) => res.json(dbLocation))
+    });
 
     // UPDATES LOCATION
     app.put('/api/locations', (req, res) => { });
 
     // DELETE LOCATION
-    app.delete('/api/locations/:id', (req, res) => { });
+    app.delete('/api/locations/:id', (req, res) => { 
+        db.Location.destroy({
+            where: {
+                id: req.params.id,
+            },
+        }).then((dbLocation) => res.json(dbLocation))
+    });
 
 };
--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -11,9 +11,19 @@ module.exports = (app) => {
     app.get('/api/users/:id', (req, res) => {});
 
     // CREATE NEW USER
-    app.post('/api/users', (req, res) => {});
+    app.post('/api/users', (req, res) => {
+        db.User.create(req.body).then((dbUser) => res.json(dbUser));
+    });
 
     // DELETE USER
-    app.delete('/api/users/:id', (req, res) => {});
+    app.delete('/api/users/:id', (req, res) => {
+        db.User.destroy({
+            where: {
+                id: req.params.id,
+            },
+            include: [db.Post],
+            include: [db.Location]
+        }).then((dbUser) => res.json(dbUser))
+    });
 
 };
