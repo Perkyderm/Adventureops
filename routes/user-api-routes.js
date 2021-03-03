0c5fbeccc6d5d61c4909771fca4c1af3058ad902
Charles Zoeller <charles.zoeller1@gmail.com>
2021-03-03 12:58:08 -0700

    added some basic routes to the user api

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -1,19 +1,45 @@
-const db = require('../models');
+const db = require("../models");
 
 module.exports = (app) => {
+  // BOILER PLATE routes codes --- might change ???
 
-    // BOILER PLATE routes codes --- might change ???
+  // FIND ALL
+  app.get("/api/users", (req, res) => {});
 
-    // FIND ALL
-    app.get('/api/users', (req, res) => {});
+  // FIND ONE
+  app.get("/api/users/:id", (req, res) => {});
 
-    // FIND ONE 
-    app.get('/api/users/:id', (req, res) => {});
+  // CREATE NEW USER
+  app.post("/api/users", (req, res) => {
+    db.User.create({
+      username: req.body.username,
+      password: req.body.password,
+    })
+      .then(function () {
+        res.redirect(307, "/api/login");
+      })
+      .catch(function (err) {
+        res.status(401).json(err);
+      });
+  });
+  app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+  });
 
-    // CREATE NEW USER
-    app.post('/api/users', (req, res) => {});
-
-    // DELETE USER
-    app.delete('/api/users/:id', (req, res) => {});
-
+  app.get("/api/user_data", function (req, res) {
+    if (!req.user) {
+      // The user is not logged in, send back an empty object
+      res.json({});
+    } else {
+      // Otherwise send back the user's email and id
+      // Sending back a password, even a hashed password, isn't a good idea
+      res.json({
+        username: req.user.username,
+        id: req.user.id,
+      });
+    }
+  });
+  // DELETE USER
+  app.delete("/api/users/:id", (req, res) => {});
 };
