4aff70f4549f576a434dad1e5d9086237730cf57
Charles Zoeller <charles.zoeller1@gmail.com>
2021-03-03 12:29:31 -0700

    updated package and changed var to const

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 // Requiring necessary npm packages
-var express = require("express");
-var session = require("express-session");
+const express = require("express");
+const session = require("express-session");
 // Requiring passport as we've configured it
-var passport = require("./config/passport");
-
+const passport = require("./config/passport");
+const unirest = require("unirest");
 // Setting up port and requiring models for syncing
-var PORT = process.env.PORT || 8080;
-var db = require("./models");
+const PORT = process.env.PORT || 8080;
+const db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
-var app = express();
+const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
