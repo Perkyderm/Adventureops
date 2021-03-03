dc467f67526e240e47be425a47037a3803ae24e9
Charles Zoeller <charles.zoeller1@gmail.com>
2021-03-03 13:20:40 -0700

    added / route nd handlebars middleware

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 // We need to use sessions to keep track of our user's login status
 app.use(
   session({ secret: "asdfqwerty", resave: true, saveUninitialized: true })
@@ -21,7 +24,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Requiring our routes
-//require("./routes/html-routes.js")(app);
+require("./routes/html-routes.js")(app);
 require("./routes/post-api-routes.js")(app);
 require("./routes/location-api-routes.js")(app);
 require("./routes/user-api-routes.js")(app);
