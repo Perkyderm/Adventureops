41e31480be7b2314ec7e19539a1da33954ba487e
Charles Zoeller <charles.zoeller1@gmail.com>
2021-03-04 12:15:30 -0700

    fixed html error

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,39 +7,39 @@ module.exports = function (app) {
   app.get("/", (req, res) => {
     //If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/user");
+      return res.redirect("/user");
     }
-    res.redirect("/login");
+    return res.redirect("/login");
   });
   app.get("/login", (req, res) => {
     if (req.user) {
-      res.redirect("/user");
+      return res.redirect("/user");
     }
-    res.render("login");
+    return res.render("login");
   });
   app.get("/signup", (req, res) => {
     if (req.user) {
-      res.redirect("/user");
+      return res.redirect("/user");
     }
-    res.render("signup");
+    return res.render("signup");
   });
 
   app.get("/user", isAuthenticated, function (req, res) {
-    res.render("home", req.user);
+    return res.render("home", req.user);
   });
 
   app.get("/posts", (req, res) => {
     if (req.user) {
-      res.redirect("/user-posts");
+      return res.redirect("/user-posts");
     }
-    res.redirect("/view-posts");
+    return res.redirect("/view-posts");
   });
 
   app.get("/view-posts", (req, res) => {
-    res.render("view-posts");
+    return res.render("view-posts");
   });
 
   app.get("/user-posts", isAuthenticated, function (req, res) {
-    res.render("create-post", req.user);
+    return res.render("create-post", req.user);
   });
 };
