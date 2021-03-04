b94ab3b47f61bb5b80a21520d425848b1ddd9f2d
Charles Zoeller <charles.zoeller1@gmail.com>
2021-03-04 10:32:28 -0700

    updated signup script

--- public/assets/JS/signup-script.js
+++ public/assets/JS/signup-script.js
@@ -1,35 +1,35 @@
 $(document).ready(function () {
   // Getting references to our form and input
   var signUpForm = $("form.signup");
-  var emailInput = $("input#email-input");
+  var usernameInput = $("input#username-input");
   var passwordInput = $("input#password-input");
 
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", function (event) {
     event.preventDefault();
     var userData = {
-      email: emailInput.val().trim(),
+      username: usernameInput.val().trim(),
       password: passwordInput.val().trim(),
     };
 
-    if (!userData.email || !userData.password) {
+    if (!userData.username || !userData.password) {
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password);
-    emailInput.val("");
+    signUpUser(userData.username, userData.password);
+    usernameInput.val("");
     passwordInput.val("");
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(email, password) {
-    $.post("/api/signup", {
-      email: email,
+  function signUpUser(username, password) {
+    $.post("/api/users", {
+      username: username,
       password: password,
     })
       .then(function (data) {
-        window.location.replace("/members");
+        window.location.replace("/user");
         // If there's an error, handle it by throwing up a bootstrap alert
       })
       .catch(handleLoginErr);
