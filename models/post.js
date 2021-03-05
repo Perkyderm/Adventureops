3a9e7fa8cfd36a2baa1d4fb7e7fc5bb9e28f5e53
Charles Zoeller <charles.zoeller1@gmail.com>
2021-03-05 14:49:12 -0700

    updated model associations

--- models/post.js
+++ models/post.js
@@ -20,6 +20,11 @@ module.exports = function (sequelize, DataTypes) {
         allowNull: false,
       },
     });
+    Post.belongsTo(models.Location, {
+      foreignKey: {
+        allowNull: false,
+      },
+    });
   };
 
   return Post;
