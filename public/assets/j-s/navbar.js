3a6ce2aee1398c90c52e29fa2fa95acd4595897d
Perkyderm <ncgoedecke@gmail.com>
2021-03-08 21:02:31 -0700

    save

--- public/assets/j-s/navbar.js
+++
@@ -1,8 +0,0 @@
-//nav bar active side bar
-$(document).ready(function () {
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-        console.log('click')
-    });
-    
-});
